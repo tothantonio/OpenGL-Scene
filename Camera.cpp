0bf76eba281a4f48f7ec6c5ab4ebeebd562781b5
Toth Antonio <tothantonio000@gmail.com>
2025-11-21 13:27:34 +0200

    added camera and mouse movement

---
+++ Camera.cpp
@@ -0,0 +1,68 @@
+#include "Camera.hpp"
+
+namespace gps {
+
+    //Camera constructor
+    //Camera constructor
+    Camera::Camera(glm::vec3 cameraPosition, glm::vec3 cameraTarget, glm::vec3 cameraUp) {
+        this->cameraPosition = cameraPosition;
+        this->cameraTarget = cameraTarget;
+
+        // front direction = normalized (target - position)
+        this->cameraFrontDirection = glm::normalize(cameraTarget - cameraPosition);
+
+        // normalize up
+        this->cameraUpDirection = glm::normalize(cameraUp);
+
+        // right = front x up
+        this->cameraRightDirection = glm::normalize(glm::cross(cameraFrontDirection, cameraUpDirection));
+    }
+
+
+    //return the view matrix, using the glm::lookAt() function
+    glm::mat4 Camera::getViewMatrix() {
+        return glm::lookAt(cameraPosition, cameraTarget, cameraUpDirection);
+    }
+
+    //update the camera internal parameters following a camera move event
+    void Camera::move(MOVE_DIRECTION direction, float speed) {
+        switch (direction) {
+        case MOVE_FORWARD:
+            cameraPosition += cameraFrontDirection * speed;
+            break;
+        case MOVE_BACKWARD:
+            cameraPosition -= cameraFrontDirection * speed;
+            break;
+        case MOVE_RIGHT:
+            cameraPosition += cameraRightDirection * speed;
+            break;
+        case MOVE_LEFT:
+            cameraPosition -= cameraRightDirection * speed;
+            break;
+        }
+
+        // Update target = position + front
+        cameraTarget = cameraPosition + cameraFrontDirection;
+    }
+
+
+    //update the camera internal parameters following a camera rotate event
+    //yaw - camera rotation around the y axis
+    //pitch - camera rotation around the x axis
+    void Camera::rotate(float pitch, float yaw) {
+        glm::vec3 front;
+        front.x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
+        front.y = sin(glm::radians(pitch));
+        front.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
+
+        cameraFrontDirection = glm::normalize(front);
+
+        // Recalculate right and up
+        cameraRightDirection = glm::normalize(glm::cross(cameraFrontDirection, glm::vec3(0.0f, 1.0f, 0.0f)));
+        cameraUpDirection = glm::normalize(glm::cross(cameraRightDirection, cameraFrontDirection));
+
+        // Update target
+        cameraTarget = cameraPosition + cameraFrontDirection;
+    }
+
+}
